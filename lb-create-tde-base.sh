7d1551ec5f8b78496bd2d3576cd2c69856c4acf3
linuxbuh <ztime@yandex.ru>
2021-04-13 10:17:40 +0300

    Update lb-create-tde-base.sh

--- lb-create-tde-base.sh
+++ lb-create-tde-base.sh
@@ -5,7 +5,7 @@ cl-builder-update --id lb-tde-base --scan ON -f
 chroot /run/calculate/mount/lb-tde-base
 
 cd /tmp && wget https://raw.githubusercontent.com/linuxbuh/create-lb-distr/main/create-lb-distr.sh
-bash /tmp/create-lb-distr.sh -lb-tde-base -lb-base-desktop
+bash /tmp/create-lb-distr.sh -lb-base-desktop -lb-tde-base 
 exit
 
 cl-builder-image --id lb-tde-base -V ON --keep-tree OFF  -v ON --image /var/calculate/linux/lb-tde-base-`date +%Y%m%d`-x86_64.iso -f
