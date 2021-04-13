104d67119749537519b4e4ee3d56c33286bee892
linuxbuh <ztime@yandex.ru>
2021-04-13 08:05:19 +0300

    add

---
+++ lb-create-base-desktop.sh
@@ -0,0 +1,11 @@
+cl-builder-prepare --source /home/guest/iso/cls-20210405-x86_64.iso --id lb-base-desktop -f -v ON 
+
+cl-builder-update --id lb-base-desktop --scan ON -f
+
+chroot /run/calculate/mount/lb-base-desktop
+
+cd /tmp && wget https://raw.githubusercontent.com/linuxbuh/create-lb-distr/main/create-lb-distr.sh
+bash /tmp/create-lb-distr.sh -lb-base-desktop
+exit
+
+cl-builder-image --id lb-base-desktop -V ON --keep-tree OFF  -v ON --image /var/calculate/linux/lb-base-desktop-`date +%Y%m%d`-x86_64.iso -f
---
+++ lb-create-tde-base.sh
@@ -0,0 +1,11 @@
+cl-builder-prepare --source /home/guest/iso/cls-20210405-x86_64.iso --id lb-tde-base -f -v ON 
+
+cl-builder-update --id lb-tde-base --scan ON -f
+
+chroot /run/calculate/mount/lb-tde-base
+
+cd /tmp && wget https://raw.githubusercontent.com/linuxbuh/create-lb-distr/main/create-lb-distr.sh
+bash /tmp/create-lb-distr.sh -lb-tde-base -lb-base-desktop
+exit
+
+cl-builder-image --id lb-tde-base -V ON --keep-tree OFF  -v ON --image /var/calculate/linux/lb-tde-base-`date +%Y%m%d`-x86_64.iso -f
