cc48e28e87ba41bf574e7529663b29d52249cfac
linuxbuh <ztime@yandex.ru>
2021-04-28 11:00:07 +0300

    Update lb-create-lxde.sh

--- lb-create-lxde.sh
+++ lb-create-lxde.sh
@@ -3,14 +3,14 @@
 #cl-builder-prepare --source /home/guest/iso/cls-20210405-x86_64.iso --id lb-lxde -f -v ON 
 cl-builder-prepare --source /var/calculate/linux/lb-base-desktop-20210414-x86_64.iso --id lb-lxde -f -v ON 
 
-cl-builder-update --id lb-lxde --scan ON -f -o
+cl-builder-update --id lb-lxde --scan ON -f -o -C ON
 
 chroot /run/calculate/mount/lb-lxde
 rm -R /var/db/repos/linuxbuh/*
 emerge --sync linuxbuh
 exit
 
-cl-builder-update --id lb-lxde --scan ON -f -o
+cl-builder-update --id lb-lxde --scan ON -f -o -C ON
 
 chroot /run/calculate/mount/lb-lxde
 
