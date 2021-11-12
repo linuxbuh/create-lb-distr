180db6898a5745c9e412c40aa26cf77a13b7a116
linuzbuh <linuxbuh@yandex.ru>
2021-11-13 02:24:27 +0300

    update

---
+++ lb-update-base-desktop.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+chroot /run/calculate/mount/lb-base-desktop /bin/bash -x <<'EOF'
+shopt -s extglob
+rm -r /var/db/repos/!(@(calculate|distros))
+exit
+EOF
+
+cl-builder-update --id lb-base-desktop --scan ON -f -o -C ON
+
+cl-builder-image --id lb-base-desktop -V ON --keep-tree OFF  -v ON --image /var/calculate/linux/lb-base-desktop-`date +%Y%m%d`-x86_64.iso -f
