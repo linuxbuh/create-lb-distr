909a2f1be164ab5e372a3754765608b191c26989
linuzbuh <linuxbuh@yandex.ru>
2021-12-09 19:19:04 +0300

    update

--- lb-create-tde-base.sh
+++ lb-create-tde-base.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-cl-builder-prepare --source /var/calculate/linux/lb-base-desktop-20210505-x86_64.iso --id lb-tde-base -f -v ON 
+#cl-builder-prepare --source /var/calculate/linux/lb-base-desktop-20210505-x86_64.iso --id lb-tde-base -f -v ON 
+cl-builder-prepare --source /var/calculate/linux/lb-base-desktop-20211128-x86_64.iso --id lb-tde-base -f -v ON 
 
-chroot /run/calculate/mount/lb-tde-base
+chroot /run/calculate/mount/lb-tde-base /bin/bash -x <<'EOF'
 shopt -s extglob
 rm -r /var/db/repos/!(@(calculate|distros))
 exit
+EOF
+
 
 cl-builder-update --id lb-tde-base --scan ON -f -o -C ON
 
-chroot /run/calculate/mount/lb-tde-base
+chroot /run/calculate/mount/lb-tde-base /bin/bash -x <<'EOF'
 
 cd /tmp && wget https://raw.githubusercontent.com/linuxbuh/create-lb-distr/main/create-lb-distr.sh
-bash /tmp/create-lb-distr.sh -lb-tde-base 
+bash /tmp/create-lb-distr.sh -lb-tde-base
 exit
+EOF
+
 
 cl-builder-image --id lb-tde-base -V ON --keep-tree OFF  -v ON --image /var/calculate/linux/lb-tde-base-`date +%Y%m%d`-x86_64.iso -f
---
+++ lb-update-tde-base.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+chroot /run/calculate/mount/lb-tde-base /bin/bash -x <<'EOF'
+shopt -s extglob
+rm -r /var/db/repos/!(@(calculate|distros))
+exit
+EOF
+
+
+cl-builder-update --id lb-tde-base --scan ON -f -o -C ON
+
+cl-builder-image --id lb-tde-base -V ON --keep-tree OFF  -v ON --image /var/calculate/linux/lb-tde-base-`date +%Y%m%d`-x86_64.iso -f
