e9f2a85a300af1b28f9a2123bcaac3a3e5b1649d
linuzbuh <linuxbuh@yandex.ru>
2022-09-20 00:07:09 +0300

    update

---
+++ lb-create-desktop1.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ -n "$1" ]
+then
+
+DATE=$1
+
+cl-builder-break --id lb-base-desktop -f
+
+cl-builder-prepare --source /var/calculate/linux/cls-$DATE-x86_64.iso --id lb-base-desktop -f -v ON
+
+cl-builder-update --id lb-base-desktop --scan ON -f -o -C ON
+
+chroot /run/calculate/mount/lb-base-desktop /bin/bash -x <<'EOF'
+cl-core-variables --set update.cl_update_other_set=on
+eselect repository add linuxbuh git https://github.com/linuxbuh/linuxbuh.git
+eix-sync && eix-update
+cl-update --scan ON -f -o -C ON
+eix-sync && eix-update
+eselect profile set 57
+cl-update-profile
+cl-update --rebuild-world
+#cd /tmp && wget https://raw.githubusercontent.com/linuxbuh/create-lb-distr/main/create-lb-distr.sh
+#bash /tmp/create-lb-distr.sh -lb-add-linuxbuh -lb-base-desktop
+exit
+EOF
+
+cl-builder-update --id lb-base-desktop --scan ON -f -o -C ON
+
+#cl-builder-image --id lb-base-desktop -V ON --keep-tree OFF  -v ON --image /var/calculate/linux/lb-base-desktop-`date +%Y%m%d`-x86_64.iso -f
+
+else
+echo "Не введена дата исходного дистрибутива CLS. Например 20211212"
+fi
